a6095eb4bf2b5a7a3fda14e1b79598b87fed5222
K.M.Chamith Dilshan <dilshankmc91@gmail.com>
2025-03-27 23:04:46 +0530

    Complete till 143 lecture

---
+++ Frontend/18/JSconstructor/script.js
@@ -0,0 +1,9 @@
+function Houskeeper(YearsOfExperience,name,cleaningRepotire){
+    this.YearsOfExperience =YearsOfExperience;
+    this.name = name;
+    this.cleaningRepotire = cleaningRepotire;
+
+    this.clean = function (){
+        alert("Cleaning in process..");
+    }
+}
