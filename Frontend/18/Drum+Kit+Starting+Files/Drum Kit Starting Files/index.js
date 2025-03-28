e1033f87ca49496ae62e242d98aae960ee3ae01d
K.M.Chamith Dilshan <dilshankmc91@gmail.com>
2025-03-28 16:42:12 +0530

    Complete till 144 lecture

--- Frontend/18/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Frontend/18/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -5,61 +5,66 @@ for(var i=0; i<numberOfDrumButtons; i++){
 
         var buttonInnerHtml = this.innerHTML;
 
-
-        switch(buttonInnerHtml){
-            case "w":
-                var audio = new Audio("sounds/crash.mp3");
-                audio.play();
-                break;
-
-            case "a":
-                var kick = new Audio("sounds/kick-bass.mp3");
-                kick.play();
-                break;
-
-            case "s":
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play();
-                break;
-
-            case "d":
-                    var tom1 = new Audio("sounds/tom-1.mp3");
-                    tom1.play();
-                    break;
-
-
-            case "j":
-                var tom2 = new Audio("sounds/tom-2.mp3");
-                tom2.play();
-                break;
-
-            case "k":
-                var tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play();
-                break;
-
-            case "l":
-                var tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play();
-                break;
-
-            default:
-                console.log(buttonInnerHtml);
-        }
-        
-        
-       
-
-       
-
+        makeSound(buttonInnerHtml);
 
 
     });
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+
+
+    
 }
 
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+});
 
 
+
+
+
+
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+    
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+    
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+    
+        case "d":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+    
+    
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+    
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+    
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+    
+        default:
+            console.log(buttonInnerHtml);
+    }
+}
 
 
